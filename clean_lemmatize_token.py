da15d63d45233c26ab71f134a86f8514bf3c9973
William Newton <will4856@gmail.com>
2020-06-22 13:43:06 -0400

    modified cleaning function to remove more punctuation and stop words

--- clean_lemmatize_token.py
+++ clean_lemmatize_token.py
@@ -1,8 +1,11 @@
 def clean_lemmatize_token(tweet):
-    cleaned_tweet = re.sub(r"""['/,.;@#?!&$]+\ *""", " ", tweet, flags= re.VERBOSE).lower()
+    stop_words = set(stopwords.words('english'))
+    cleaned_tweet = tweet.translate(str.maketrans('', '', string.punctuation)).lower()
     tokenized_tweet = word_tokenize(cleaned_tweet)
+    filtered_tweet = [w for w in tokenized_tweet if not w in stop_words]
     lemmatizer = WordNetLemmatizer()
     lemmatized_tweet = []
-    for word in tokenized_tweet:
+    for word in filtered_tweet:
         lemmatized_tweet.append(lemmatizer.lemmatize(word))
     return lemmatized_tweet
+
