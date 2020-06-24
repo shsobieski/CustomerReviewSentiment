eb9257d8aa82988f1ce9a975d2551c95e8072c02
William Newton <will4856@gmail.com>
2020-06-24 14:31:21 -0400

    fixed function to remove hashtags and remove tweet specific words

--- clean_lemmatize_token.py
+++ clean_lemmatize_token.py
@@ -1,21 +1,13 @@
 def clean_lemmatize_token(tweet):
     stop_words = set(stopwords.words('english'))
-    cleaned = tweet.translate(str.maketrans('', '', '!"$%&\'()*+,-./:;<=>?@[\\]^_`{|}~')).lower()
+    cleaned = tweet.translate(str.maketrans('', '', string.punctuation)).lower()
     tokenized = word_tokenize(cleaned)
     filtered = [w for w in tokenized if not w in stop_words]
     lemmatizer = WordNetLemmatizer()
     lemmatized = []
     for word in filtered:
         lemmatized.append(lemmatizer.lemmatize(word))
-    for word in lemmatized:
-        if word == '#':
-            index = lemmatized.index('#')
-            next_word = lemmatized[index + 1]
-            joined = word + next_word
-            list_to_remove = [next_word]
-            lemmatized = [w for w in lemmatized if w not in list_to_remove]
-            lemmatized.append(joined)
-    to_remove = ['#']
+    to_remove = ['rt','mention','sxsw','link']
     lemmatized = [w for w in lemmatized if w not in to_remove]
     lemmatized = ' '.join(lemmatized)
     return lemmatized
